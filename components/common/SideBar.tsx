60b4dbc4546cd000290636f01c23e15818d96517
[SW5] 홍승진 <cmikal47@gmail.com>
2024-03-23 08:37:33 +0900

    Header, aside 레이아웃 수정(sticky)

--- components/common/SideBar.tsx
+++ components/common/SideBar.tsx
@@ -6,24 +6,26 @@ import { getTrendKeywords } from '@/service/server-actions';
 const SideBar = async () => {
   const trendKeywords = await getTrendKeywords();
   return (
-    <aside className={styles.sidebar}>
-      <div className={`${styles.card} ${styles.info}`}>
-        <h3>
-          <InfoIcon />
-          Notice
-        </h3>
-        <p>
-          • '원본 박물관 탐색기'는 유튜브 자동생성 자막에 의존합니다. 검색
-          결과가 정확하지 않을 수 있습니다.
-        </p>
-        <p>• 2024, 2023, 2022 자료까지 업데이트됐습니다.</p>
-      </div>
-      {trendKeywords.length > 0 && (
-        <div className={`${styles.card}`}>
-          <h3>인기 키워드</h3>
-          <TrendKeywords trendKeywords={trendKeywords} />
+    <aside>
+      <div className={styles.sidebar}>
+        <div className={`${styles.card} ${styles.info}`}>
+          <h3>
+            <InfoIcon />
+            Notice
+          </h3>
+          <p>
+            • '원본 박물관 탐색기'는 유튜브 자동생성 자막에 의존하기 때문에 검색
+            결과가 정확하지 않을 수 있습니다.
+          </p>
+          <p>• 2024 자료까지 업데이트됐습니다.</p>
         </div>
-      )}
+        {trendKeywords.length > 0 && (
+          <div className={`${styles.card}`}>
+            <h3>인기 키워드</h3>
+            <TrendKeywords trendKeywords={trendKeywords} />
+          </div>
+        )}
+      </div>
     </aside>
   );
 };
