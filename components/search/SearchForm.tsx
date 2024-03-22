adbc3f81b8317cf83b45ac6a83b24c76d38d13ac
[SW5] 홍승진 <cmikal47@gmail.com>
2024-03-23 02:34:21 +0900

    layout 수정

--- components/search/SearchForm.tsx
+++ components/search/SearchForm.tsx
@@ -8,10 +8,9 @@ import { useSearchParams, useRouter } from 'next/navigation';
 
 const SearchForm = () => {
   const [year, setYear] = useState(YEAR_FILTER_LIST[0]);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const router = useRouter();
   const searchParams = useSearchParams();
-  const keyword = searchParams.get('keyword') || '';
+  const [keyword, setKeyword] = useState(searchParams.get('keyword') || '');
+  const router = useRouter();
 
   const handleChangeFilter = (year: string) => {
     setYear(year);
@@ -19,23 +18,24 @@ const SearchForm = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    if (!inputRef.current) return;
-    if (inputRef.current.value.length < 1) return;
-    const sanitizedKeyword = encodeURIComponent(inputRef.current?.value);
+    if (!keyword) return;
+    if (keyword.length < 1) return;
+    const sanitizedKeyword = encodeURIComponent(keyword);
     router.push(`/search/?keyword=${sanitizedKeyword}&year=${year}`);
   };
 
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.value = keyword;
-    }
-  }, [keyword]);
-
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <input placeholder="키워드 검색" ref={inputRef} />
+      <input
+        placeholder="키워드 검색"
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        autoComplete="on"
+        name="keyword"
+        type="text"
+      />
       <YearFilter onChangeYear={handleChangeFilter} year={year} />
-      <button type="submit" aria-label="검색">
+      <button type="submit" aria-label="검색" disabled={!keyword}>
         <svg width={24} viewBox="0 0 24 24" aria-hidden="true">
           <g>
             <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
