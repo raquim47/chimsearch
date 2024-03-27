783c85a0b92ce3f3f2c72d7314c1b8b6571a012c
[SW5] 홍승진 <cmikal47@gmail.com>
2024-03-27 17:16:07 +0900

    Fix deploy error: server actions

--- components/common/SideBar.tsx
+++ components/common/SideBar.tsx
@@ -1,6 +1,7 @@
 import styles from './SideBar.module.css';
 import TrendKeywords from './TrendKeywords';
 import { getTrendKeywords } from '@/services/server-actions';
+import { TrendKeywordI } from '@/utils/types';
 
 const SideBar = async () => {
   const trendKeywords = await getTrendKeywords();
@@ -10,15 +11,15 @@ const SideBar = async () => {
         <div className={`${styles.card} ${styles.info}`}>
           <h3>알림</h3>
           <p>
-            • &apos;원본 박물관 탐색기&apos;는 유튜브의 자동 생성 자막에 기반합니다. 검색
-            결과는 다소 정확하지 않을 수 있습니다.
+            • &apos;원본 박물관 탐색기&apos;는 유튜브의 자동 생성 자막에
+            기반합니다. 검색 결과는 다소 정확하지 않을 수 있습니다.
           </p>
           <p>• 2024, 2023 자료까지 업데이트 되었습니다.</p>
         </div>
-        {trendKeywords.length > 0 && (
+        {trendKeywords && trendKeywords.length > 0 && (
           <div className={`${styles.card}`}>
             <h3>인기 키워드</h3>
-            <TrendKeywords trendKeywords={trendKeywords} />
+            <TrendKeywords trendKeywords={trendKeywords as TrendKeywordI[]} />
           </div>
         )}
       </div>
--- services/server-actions.ts
+++ services/server-actions.ts
@@ -1,14 +1,25 @@
-import { TrendKeywordI } from "@/utils/types";
+import { connectToDatabase } from './mongodb';
 
-export const getTrendKeywords = async (): Promise<TrendKeywordI[]> => {
-  const res = await fetch(`/api/trend-keywords`, {
-    next: {
-      revalidate: 300,
-    },
-  });
-  if (!res.ok) {
-    throw new Error('Failed to fetch Trend Keywords');
+export const getTrendKeywords = async () => {
+  'use server';
+
+  try {
+    const db = await connectToDatabase();
+    const keywordsCollection = db.collection('searchKeywords');
+    const result = await keywordsCollection
+      .find({})
+      .sort({ count: -1 })
+      .limit(5)
+      .toArray();
+
+    const trendKeywords = result.map((doc) => ({
+      _id: doc._id.toString(), 
+      keyword: doc.keyword,
+      count: doc.count,
+    }));
+    return trendKeywords;
+  } catch (error) {
+    console.error('Failed to fetch trend keywords:', error);
+    throw new Error('Failed to fetch trend keywords');
   }
-  const data = await res.json();
-  return data.trendKeywords;
 };
